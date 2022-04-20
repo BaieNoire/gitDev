774f1a5295283e1736b08d39c5ed2883e524ed77
BaieNoire <crypto.dreadbob@gmail.com>
2022-04-20 12:15:48 +0200

    Vesrion 0.1 gitDev 20/04 12h15

---
+++ Dev/main.py
@@ -0,0 +1,9 @@
+"""
+Création programme simple V2
+
+"""
+
+nom_personne = input("Quel est votre nom ?")
+print(f"Bonjour {nom_personne} !")
+age_personne = input("Quel age avez vous ?")
+print(f"Vous avez {age_personne} ans {nom_personne} ? \nN'est-ce pas ?\n\u2764")
